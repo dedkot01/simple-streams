d932f4afb4a03d3d49f9dd04f93dc91b54d16730
Zhdanov <dedkot01@gmail.com>
2021-05-13 13:11:01 +0300

    added simple example from QuickStart guide

--- akka-stream/src/main/scala/dedkot/Main.scala
+++ akka-stream/src/main/scala/dedkot/Main.scala
@@ -1,7 +1,31 @@
 package dedkot
 
 import akka.actor.ActorSystem
+import akka.stream.IOResult
+import akka.stream.scaladsl.{ FileIO, Source }
+import akka.util.ByteString
+import akka.{ Done, NotUsed }
 
-object Main extends App {
-  implicit val system: ActorSystem = ActorSystem("QuickStart")
+import java.nio.file.Paths
+import scala.concurrent.{ ExecutionContextExecutor, Future }
+
+object SimpleSourceExample extends App {
+  implicit val system: ActorSystem          = ActorSystem("QuickStart")
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
+
+  val source: Source[Int, NotUsed] = Source(1 to 100)
+  val done: Future[Done]           = source.runForeach(println)
+  done.onComplete(_ => system.terminate())
+}
+
+object FactorialsInFileExample extends App {
+  implicit val system: ActorSystem          = ActorSystem("QuickStart")
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
+
+  val source     = Source(1 to 100)
+  val factorials = source.scan(BigInt(1))((acc, next) => acc * next)
+  val result: Future[IOResult] = {
+    factorials.map(num => ByteString(s"$num\n")).runWith(FileIO.toPath(Paths.get("factorials.txt")))
+  }
+  result.onComplete(_ => system.terminate())
 }
