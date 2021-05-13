5e1a9f03934046da6420ff552e850f87d42b1d56
Zhdanov <dedkot01@gmail.com>
2021-05-13 17:43:11 +0300

    added simple transform data from files in directory

--- akka-stream/src/main/scala/dedkot/SimpleTransformData.scala
+++ akka-stream/src/main/scala/dedkot/SimpleTransformData.scala
@@ -1,9 +1,13 @@
 package dedkot
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{ Flow, Sink, Source }
+import akka.stream.alpakka.csv.scaladsl.{ CsvParsing, CsvToMap }
+import akka.stream.alpakka.file.scaladsl.Directory
+import akka.stream.scaladsl.{ FileIO, Flow, Sink, Source }
 import akka.{ Done, NotUsed }
 
+import java.nio.charset.StandardCharsets
+import java.nio.file.{ FileSystems, Path }
 import scala.concurrent.{ ExecutionContextExecutor, Future }
 
 case class BadRecord(name: String, age: Int)
@@ -34,4 +38,25 @@ object SimpleTransformData extends App {
     .via(flow)
     .runWith(sink)
     .onComplete(_ => system.terminate())
+}
+
+object SimpleTransformDataFromFile extends App {
+  implicit val system: ActorSystem          = ActorSystem("SimpleTransformDataFromFile")
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
+
+  val path = FileSystems.getDefault.getPath(".\\test-data")
+
+  val directory: Source[Path, NotUsed] = Directory.ls(path)
+  val files: Flow[Path, Map[String, String], NotUsed] = Flow[Path].flatMapConcat { path =>
+    FileIO.fromPath(path).via(CsvParsing.lineScanner()).via(CsvToMap.toMapAsStrings(StandardCharsets.UTF_8))
+  }
+  val records: Flow[Map[String, String], BadRecord, NotUsed] = Flow[Map[String, String]].map { record =>
+    BadRecord(record("name"), record("age").toInt)
+  }
+  val transform: Flow[BadRecord, GoodRecord, NotUsed] = Flow[BadRecord].map { record =>
+    val names = record.name.split(" ")
+    GoodRecord(names(0), names(1), record.age)
+  }
+
+  directory.via(files).via(records).via(transform).runForeach(println)
 }
--- build.sbt
+++ build.sbt
@@ -1,12 +1,14 @@
 ThisBuild / organization := "dedkot"
-ThisBuild / version      := "0.1"
+ThisBuild / version := "0.1"
 ThisBuild / scalaVersion := "2.13.5"
 
 val AkkaVersion = "2.6.14"
 lazy val akkaStream = (project in file("akka-stream"))
   .settings(
     libraryDependencies ++= Seq(
-      "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
-      "com.typesafe.akka" %% "akka-stream-testkit" % AkkaVersion % Test
+      "com.lightbend.akka" %% "akka-stream-alpakka-file" % "2.0.2",
+      "com.lightbend.akka" %% "akka-stream-alpakka-csv"  % "2.0.2",
+      "com.typesafe.akka"  %% "akka-stream"              % AkkaVersion,
+      "com.typesafe.akka"  %% "akka-stream-testkit"      % AkkaVersion % Test
     )
   )
