2e99c50683819a3ac7e7846382ce4b068dfec303
dedkot <dedkot01@gmail.com>
2021-05-26 13:54:06 +0300

    window worked

--- flink-stream/src/main/scala/dedkot/WindowStream.scala
+++ flink-stream/src/main/scala/dedkot/WindowStream.scala
@@ -1,9 +1,11 @@
 package dedkot
 
-import org.apache.flink.api.common.eventtime.WatermarkStrategy
+import org.apache.flink.api.common.eventtime.{ SerializableTimestampAssigner, WatermarkStrategy }
 import org.apache.flink.streaming.api.scala.{ createTypeInformation, StreamExecutionEnvironment }
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
+
+import java.time.Duration
 
 object WindowStream extends App {
   val env = StreamExecutionEnvironment.getExecutionEnvironment
@@ -12,9 +14,15 @@ object WindowStream extends App {
 
   env
     .fromCollection(numbers)
-    .ass
+    .assignTimestampsAndWatermarks(
+      WatermarkStrategy
+        .forBoundedOutOfOrderness[(Int, Int)](Duration.ofSeconds(15))
+        .withTimestampAssigner(new SerializableTimestampAssigner[(Int, Int)] {
+          override def extractTimestamp(element: (Int, Int), recordTimestamp: Long): Long = element._1
+        })
+    )
     .keyBy(_._1)
-    .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+    .window(TumblingEventTimeWindows.of(Time.seconds(15)))
     .reduce { (v1, v2) =>
       (v1._1, v1._2 + v2._2)
     }
